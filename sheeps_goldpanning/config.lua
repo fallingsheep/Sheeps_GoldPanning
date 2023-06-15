059dee516cfee4eb99a1bd79311c2487512add29
fallingsheep <razorsoftinfo@gmail.com>
2023-06-15 19:29:35 +1000

    fixes

--- sheeps_goldpanning/config.lua
+++ sheeps_goldpanning/config.lua
@@ -2,20 +2,23 @@ Config = {}
 -- text messages
  Config.gold_no_collect     = 'You can\'t find gold here.'
  Config.gold_not_found      = 'You didn\'t found anything.'
- Config.found_normal = "You found something."
- Config.found_valuable = "You found something valuable."
- Config.found_rare = "You found something rare."
- Config.found_very_rare = "You found something very rare."
+ Config.found_normal        = "You found something."
+ Config.found_valuable      = "You found something valuable."
+ Config.found_rare          = "You found something rare."
+ Config.found_very_rare     = "You found something very rare."
+
+ -- search time (default 10000 - 10seconds)
+ Config.waittime = 10000
 
  --item chances (makre it covers the range 1-100)
- Config.chance_nothing = {1, 22}   -- 22%
- Config.chance_normal = {23, 77}  -- 55%
- Config.chance_valuable = {78, 92} -- 15%
- Config.chance_rare = {93, 97}  -- 5%
- Config.chance_very_rare = {98, 100}  -- 3%
+ Config.chance_nothing      = {1, 22}   -- 22%
+ Config.chance_normal       = {23, 77}  -- 55%
+ Config.chance_valuable     = {78, 92} -- 15%
+ Config.chance_rare         = {93, 97}  -- 5%
+ Config.chance_very_rare    = {98, 100}  -- 3%
 
 -- items that can be found
-Config.item_normal = {"fishbait","nails","rock","shellcasing","glasseye","provision_db_finger_bone","bolts","metal_gear","provision_arrowhead_bone","provision_arrowhead_chipped","provision_arrowhead_crude","provision_arrowhead_feldspar","provision_arrowhead_granite","provision_arrowhead_rough","provision_arrowhead_splintered"}
-Config.item_valuable = {"goldnugget","quartz","provision_arrowhead_quartz","provision_arrowhead_obsidian","provision_arrowhead_agate"}
-Config.item_rare = {"provision_coin_1700_ny_tkn","provision_coin_1787_cent_tkn","provision_coin_1789_pny","provision_coin_1792_lib_qtr","provision_coin_1792_nickel","provision_coin_1792_qtr","provision_coin_1794_slv_dlr","provision_coin_1795_hlf_eag","provision_coin_1796_hlf_pny","provision_coin_1797_gld_eag","provision_coin_1798_slv_dlr","provision_coin_1800_five_dlr","provision_coin_1800_gld_dlr","provision_coin_1800_gld_qtr","provision_coin_1800_hlf_dime"}
-Config.item_very_rare = {"provision_asteroid_chunk","diamond","emerald","pearl"}
+Config.item_normal      = {"fishbait","nails","rock","shellcasing","glasseye","provision_db_finger_bone","bolts","metal_gear","provision_arrowhead_bone","provision_arrowhead_chipped","provision_arrowhead_crude","provision_arrowhead_feldspar","provision_arrowhead_granite","provision_arrowhead_rough","provision_arrowhead_splintered"}
+Config.item_valuable    = {"goldnugget","quartz","provision_arrowhead_quartz","provision_arrowhead_obsidian","provision_arrowhead_agate"}
+Config.item_rare        = {"provision_coin_1700_ny_tkn","provision_coin_1787_cent_tkn","provision_coin_1789_pny","provision_coin_1792_lib_qtr","provision_coin_1792_nickel","provision_coin_1792_qtr","provision_coin_1794_slv_dlr","provision_coin_1795_hlf_eag","provision_coin_1796_hlf_pny","provision_coin_1797_gld_eag","provision_coin_1798_slv_dlr","provision_coin_1800_five_dlr","provision_coin_1800_gld_dlr","provision_coin_1800_gld_qtr","provision_coin_1800_hlf_dime"}
+Config.item_very_rare   = {"provision_asteroid_chunk","diamond","emerald","pearl"}
--- sheeps_goldpanning/server.lua
+++ sheeps_goldpanning/server.lua
@@ -31,22 +31,22 @@ AddEventHandler("search", function()
     local chance =  math.random(1, 100)
     local randomIndex = math.random(#Config.item_normal)
     local randomItem = Config.item_normal[randomIndex]
-    local randomIndex2 = math.random(#Config.chance_valuable)
-    local randomItem2 = Config.chance_valuable[randomIndex]
-    local randomIndex3 = math.random(#Config.chance_rare)
-    local randomItem3 = Config.chance_rare[randomIndex]
-    local randomIndex4 = math.random(#Config.chance_very_rare)
-    local randomItem4 = Config.chance_very_rare[randomIndex]
+    local randomIndex2 = math.random(#Config.item_valuable)
+    local randomItem2 = Config.item_valuable[randomIndex]
+    local randomIndex3 = math.random(#Config.item_rare)
+    local randomItem3 = Config.item_rare[randomIndex]
+    local randomIndex4 = math.random(#Config.item_very_rare)
+    local randomItem4 = Config.item_very_rare[randomIndex]
 
-    if chance >= Config.chance_nothing[0] and chance <= Config.chance_nothing[1] then
+    if chance >= Config.chance_nothing[1] and chance <= Config.chance_nothing[2] then
         TriggerClientEvent("vorp:TipBottom", _source, Config.gold_not_found, 6000)
-    elseif chance >= Config.chance_normal[0] and chance <= Config.chance_normal[1] then
+    elseif chance >= Config.chance_normal[1] and chance <= Config.chance_normal[2] then
         GiveItem(_source, randomItem, Config.found_normal)
-    elseif chance >= Config.chance_valuable[0] and chance <= Config.chance_valuable[1] then
+    elseif chance >= Config.chance_valuable[1] and chance <= Config.chance_valuable[2] then
         GiveItem(_source, randomItem2, Config.found_valuable)
-    elseif chance >= Config.chance_rare[0] and chance <= Config.chance_rare[1] then
+    elseif chance >= Config.chance_rare[1] and chance <= Config.chance_rare[2] then
         GiveItem(_source, randomItem3, Config.found_rare)
-    elseif chance >= Config.chance_very_rare[0] and chance <= Config.chance_very_rare[1] then
+    elseif chance >= Config.chance_very_rare[1] and chance <= Config.chance_very_rare[2] then
         GiveItem(_source, randomItem4, Config.chance_very_rare)
     else
         --fallback
@@ -56,7 +56,7 @@ AddEventHandler("search", function()
 end)
 
 function GiveItem(source, item, message)
-    VorpInv.addItem(source, item[0], 1)
+    VorpInv.addItem(source, item, 1)
     Wait(100)
     TriggerClientEvent("vorp:TipBottom", source, message, 6000)
 end
